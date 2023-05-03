01531de85b2eb14dae26a224545c21570beb02f2
Abhishek Roy <abhishek.roy@perspectum.com>
2023-05-03 18:16:54 +0100

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    print_labeled_measurement(5, 'h');
+    expression();
+    let increment_result = increment(10);
+    println!("The value of increment_result is: {increment_result}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn expression() {
+    let y = {
+        let x = 3;
+        x + 1 // expression does not end with semicolon
+    };
+
+    println!("The value of y is: {y}");
+}
+
+fn increment(x: i32) -> i32 {
+    x + 1 // since there's no semicolon, this is an expression. and x+1 is returned
+}
